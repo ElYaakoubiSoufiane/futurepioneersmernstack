78a313efe938ba1cd5fe8d7fcc0bfb33b799594d
El Yaakoubi Soufiane <137622047+ElYaakoubiSoufiane@users.noreply.github.com>
2023-11-15 09:46:46 +0100

    Update Login.tsx

--- frontend/src/components/register_login/Login/Login.tsx
+++ frontend/src/components/register_login/Login/Login.tsx
@@ -45,7 +45,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const url = "http://localhost:5000/api/auth";
+      const url = "https://futurepioneersmernstack-c4ax.vercel.app/api/auth";
       const { data: res } = await axios.post(url, data);
      localStorage.setItem("token", res.data);
       // console.log(res.data);
