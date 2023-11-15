fc92f6dfeae44eae3647d7a77cae4e93964892d0
unknown <soufiane0elyaakoubi@gmail.com>
2023-11-15 12:43:01 +0100

    update

--- frontend/src/components/logout/LogOut.tsx
+++ frontend/src/components/logout/LogOut.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import {  handleLogout } from "../Context/Reducer";
-
+import { handleLogout } from "../Context/Reducer";
 
 export default async function LogOut() {
   const Navigate = useNavigate();
   const dispatch = useDispatch();
   useEffect(() => {
-    fetch("https://futurepioneersmernstack-c4ax.vercel.app/api/users/logout", {
+    fetch("https://futurepioneers.vercel.app/api/users/logout", {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -26,8 +25,6 @@ export default async function LogOut() {
         dispatch(handleLogout());
       })
       .catch((err) => console.log(err));
-
-  
   });
 
   return <div></div>;
