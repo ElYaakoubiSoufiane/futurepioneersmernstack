eda682036c4214e40b89d88ee1fdbad53b1f710c
unknown <soufiane0elyaakoubi@gmail.com>
2023-11-19 23:39:17 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -74,6 +74,7 @@ const isLogged=useSelector((state:any)=>state.auth.isLogged)
           ):(<>
             <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="*" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             </Routes>
